7f42beca8911426b3b0265b3943c5297155f81ec
tabdurahmon <dilsozxonxatamova@gmail.com>
2024-08-06 23:07:15 +0500

    add

--- src/app/singleProduct/[id]/page.js
+++ src/app/singleProduct/[id]/page.js
@@ -1,6 +1,8 @@
 "use client";
 
+import Link from "next/link";
 import { useEffect, useState } from "react";
+import { FaHome } from "react-icons/fa";
 
 const getData = async (id) => {
   const req = await fetch(`https://dummyjson.com/products/${id}`);
@@ -23,10 +25,14 @@ const SingleProduct = ({ params }) => {
   return (
     <div className="min-h-screen bg-gray-100 p-6">
       <div className="bg-white p-6 rounded-lg shadow-lg">
+        <Link href="/" className=" flex items-center text-blue-500 mb-4">
+          <FaHome className="mr-2" />
+          Back Home
+        </Link>
         <img
           src={product.thumbnail}
           alt={product.title}
-          className=" h-64 object-cover mb-4 rounded-lg"
+          className="h-64 object-cover mb-4 rounded-lg"
         />
         <h1 className="text-4xl font-bold mb-4 text-gray-800">
           {product.title}
@@ -36,14 +42,13 @@ const SingleProduct = ({ params }) => {
           <p className="text-xl font-bold text-gray-800 mb-3">
             Category: {product.category}
           </p>
-          <p className="btn text-xl font-bold text-gray-800 mb-3">
+          <p className="text-xl font-bold text-gray-800 mb-3">
             Rating: {product.rating}
           </p>
           <p className="text-xl font-bold text-gray-800 mb-3">
             Brand: {product.brand}
           </p>
         </div>
-
         <span className="text-2xl font-bold text-gray-800">
           ${product.price}
         </span>
