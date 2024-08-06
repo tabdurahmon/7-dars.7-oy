8359ecd6cd4f04c98c1ec99a6a0753984274a697
tabdurahmon <dilsozxonxatamova@gmail.com>
2024-08-06 22:48:23 +0500

    add

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 function Navbar() {
   return (
     <div className="w-full mx-auto px-5 flex justify-between items-center py-4 bg-white shadow-md">
-      <h1 className="text-3xl font-bold text-gray-800">Logo</h1>
+      <h1 className="text-3xl font-bold text-gray-800">Market</h1>
       <nav className="flex gap-6">
         <Link href="/" className="text-lg text-gray-700 hover:text-gray-900">
           Home
